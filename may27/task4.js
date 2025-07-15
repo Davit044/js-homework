d0db1af777c2c1dc64a9e6e1bd3faa900178d88d
Davit044 <davit.chilingaryan04@gmail.com>
2025-07-15 15:23:43 +0400

    Update task4.js

--- may27/task4.js
+++ may27/task4.js
@@ -6,8 +6,5 @@ for (let i = str1.length - 1; i >= 0; --i) {
 }
 
 function myReverse(str) {
-    const arr = str.split("");
-    arr.reverse();
-    const str_rev = arr.join("");
-    return str_rev;
+    return str.split("").reverse().join("");
 }
