0b42ca859069edf6cde98baabe6bc9a2a58e60de
Davit044 <davit.chilingaryan04@gmail.com>
2025-07-15 23:25:39 +0400

    Update task4.js

--- june7/task4.js
+++ june7/task4.js
@@ -1,8 +1,6 @@
 Array.prototype.my_some(callback) {
     for (let i = 0; i < this.length; i++) {
-        if (callback(this[i], i, this)) {
-            return true;
-        }
+        if (callback(this[i], i, this)) return true;
     }
     return false;
 }
