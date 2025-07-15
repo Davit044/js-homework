4690bd61903dd969209b56fa3bf91a6d39dc0f1a
Davit044 <davit.chilingaryan04@gmail.com>
2025-07-15 14:38:49 +0400

    Update task1.js

--- may27/task1.js
+++ may27/task1.js
@@ -1,10 +1,14 @@
+const str = "hello";
 
-
-function lenght(str) {
+function Mylen(str) {
 
     let count = 0;
     while (str[count] != undefined) {
         ++count;
     }
     return count;
+}
+
+function len(str) {
+    return str.length;
 }
