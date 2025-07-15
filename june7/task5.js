8c58047f9c045342889e9284c5cdabd13fea6d01
Davit044 <davit.chilingaryan04@gmail.com>
2025-07-15 18:58:49 +0400

    Update task5.js

--- june7/task5.js
+++ june7/task5.js
@@ -1,6 +1,6 @@
-function my_Every(arr, callback) {
-    for (let i = 0; i < arr.length; i++) {
-        if (!callback(arr[i], i, arr)) {
+Array.prototype.my_every(callback) {
+    for (let i = 0; i < this.length; i++) {
+        if (!callback(this[i], i, this)) {
             return false;
         }
     }
@@ -8,7 +8,7 @@ function my_Every(arr, callback) {
 }
 
 const arr = [2, 4, 6, 8];
-const evens = my_Every(arr, function (num) {
+const evens = arr.my_every(function (num) {
     return num % 2 === 0;
 });
 console.log(evens);  
